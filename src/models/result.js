28de601be4b2c03f77ef38f4360824962be08dfb
David Conner <agilebluefox@gmail.com>
2016-09-02 17:36:14 -0400

    Removed uniqueness requirement. The data in each result will be unique based on  multiple fields rather than any single property.

--- src/models/result.js
+++ src/models/result.js
@@ -7,15 +7,15 @@ const mongooseUniqueValidator = require('mongoose-unique-validator');
 let schema = new Schema( {
     // Each set of results is connected to an event that should be 
     // selected when the results file is imported
-    event: { type: Schema.Types.ObjectId, ref: 'Event', required: true, unique: true },
-    // Each result has a class selected when the results are imported so 
-    // the raw file needs to contain only the results from a single class.
-    class: { type: Schema.Types.ObjectId, ref: 'Class', required: true, unique: true },
+    event: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    // Each result has a class selected when the results are imported.
+    class: { type: Schema.Types.ObjectId, ref: 'Class', required: true },
     // A course identifier has to be added to the raw data before importing
-    // to distinguish the course for which the data applies.
+    // to distinguish the course for which the data applies or a method of 
+    // informing the app of the course parameters is needed. The app could
+    // determine the course on it's own by analyzing the data. 
     course: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
     card: { type: Schema.Types.ObjectId, ref: 'Card', required: true },
-    // How do I deal with the actual data associated with the result?
     student: { type: Schema.Types.ObjectId, ref: 'Student', required: true },
     cn: { type: String },
     time: { type: Date } 

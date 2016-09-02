99d9bbe00a46dc4647030f3853e05c17749d7e8e
David Conner <agilebluefox@gmail.com>
2016-09-02 17:38:02 -0400

    Add type property. Some controls have special roles, such as clear, start, and finish, which should be part of the object.

--- src/models/course.js
+++ src/models/course.js
@@ -2,21 +2,25 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// Require the mongoose validator to enforce the unique requirement.
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 let schema = new Schema( {
-    // More than one class can participate in an event
     location: { type: String, required: true },
     name: { type: String, required: true },
     type: { type: String, required: true },
     in_order: { type: Boolean, required: true },
-    // How do I best store the control and it's point value?
-    controls: [ {  
-        number: {type: String, required: true}, 
-        points: {type: Number, default: 0} 
-     } ]
+    controls: [ 
+        { 
+            number: { type: String, required: true, unique: true },
+            type: { type: String, required: true, unique: true },
+            points: { type: Number }
+        }
+     ]
 });
 
+
+// Register the validator using the plugin method
 schema.plugin(mongooseUniqueValidator);
 
-module.exports = mongoose.model('Event', schema);
+module.exports = mongoose.model('Course', schema);

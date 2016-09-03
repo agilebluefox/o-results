8fde77d1a148369e829b8416516899810f9cf59f
David Conner <agilebluefox@gmail.com>
2016-09-02 21:24:43 -0400

    Updated comments

--- src/models/event.js
+++ src/models/event.js
@@ -5,15 +5,13 @@ const Schema = mongoose.Schema;
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 let schema = new Schema( {
-    // The event Id can be created using a pattern based on the name and 
-    // date when the event is added to the database.
     location: { type: String, required: true },
     name: { type: String, required: true },
     date: { type: Date, required: true },
     // An event can have more than one course
-    courses: [{type: Schema.Types.ObjectId, ref: 'Course'}],
+    courses: [ {type: Schema.Types.ObjectId, ref: 'Course'} ],
     // more than one class can participate in an event
-    classes: [ { type: Schema.Types.ObjectId, ref: 'Class' }]
+    classes: [ { type: Schema.Types.ObjectId, ref: 'Class' } ]
 });
 
 schema.plugin(mongooseUniqueValidator);

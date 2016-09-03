784809f63aa6b7a8c19754c8205523a4324e38d0
David Conner <agilebluefox@gmail.com>
2016-09-02 21:24:14 -0400

    Mock test data for cards and controls

---
+++ tests/db/test-control.js
@@ -0,0 +1,107 @@
+[
+    {
+        "number": "1",
+        "type": "station",
+        "points": 0
+    },
+    {
+        "number": "2",
+        "type": "clear",
+        "points": 0
+    },
+    {
+        "number": "3",
+        "type": "start",
+        "points": 0
+    },
+    {
+        "number": "4",
+        "type": "finish",
+        "points": 0
+    },
+    {
+        "number": "81",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "82",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "83",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "84",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "85",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "86",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "87",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "88",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "89",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "90",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "91",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "92",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "93",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "94",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "95",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "96",
+        "type": "control",
+        "points": 0
+    },
+    {
+        "number": "97",
+        "type": "control",
+        "points": 0
+    }
+]

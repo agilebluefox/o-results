270272ffe278d117aa919bbad8945445e4271a8c
David Conner <agilebluefox@gmail.com>
2016-09-04 20:53:13 -0400

    Remove beforeEach hook

--- tests/setup-tests.js
+++ tests/setup-tests.js
@@ -7,22 +7,17 @@ mongoose.Promise = require('bluebird');
 const mongodb = require('mongodb');
 const uri = 'mongodb://localhost:27017/o-results-test';
 
-// Make a connection to the database
-beforeEach(function (done) {
-
-    if (mongoose.connection.readyState === 0) {
-        mongoose.connect(uri, function (error) {
-            if (error) {
-                console.log('Connection failed.');
-                process.exit(1);
-            }
-        });
-    }
-    done();
-});
+if (mongoose.connection.readyState === 0) {
+    mongoose.connect(uri, function (error) {
+        if (error) {
+            console.log('Connection failed.');
+            process.exit(1);
+        }
+    });
+}
 
 // After the tests, disconnect from the db
-afterEach(function (done) {
+after(function (done) {
     mongoose.disconnect();
     return done();
 });

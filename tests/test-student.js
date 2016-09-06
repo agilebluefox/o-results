8854df4149ea0e101a01fd3155970a46cc096e59
David Conner <agilebluefox@gmail.com>
2016-09-05 22:51:41 -0400

    Ensure data loads before tests run. I added a conditional to the function that inserts the documents which prevents the done function from being called until all of the data is loaded.

--- tests/test-student.js
+++ tests/test-student.js
@@ -13,7 +13,8 @@ const Student = require('../src/models/student');
 const data = require('./data/test-student.json');
 
 // Function to insert a student into the Student collection
-function addStudent(data) {
+function addStudent(data, class_id, done) {
+    let count = 0;
     data.forEach(function(student) {
         Student.create({
             "unityid": student.unityid,
@@ -21,32 +22,48 @@ function addStudent(data) {
             "firstname": student.firstname,
             "lastname": student.lastname,
             "sex": student.sex,
-            "class": student.class
+            "class": [class_id]
         }, function(error, student) {
             if (error) console.log(error);
+            // Use conditional to make sure all the documents are
+            // stored before the done function is returned.
+            // This prevents running additional tests until the
+            // collection is loaded completely.
+            count += 1;
+            if (count === data.length ) return done();
         });
     });
-    return;
 }
 
 describe('Student collection: ', function() {
     // Clear the collection before running the tests
-    before(function(done) {
+    before('Empty the student collection', function(done) {     
         Student.remove().exec();
+        Student.find({}, function(error, students) {
+            if (error) console.log(error);
+        });
+        return done();       
+    });
+
+    // Insert the test data into the student collection
+    before('Load the student collection', function(done) {        
         // Get a class to add to the students
-        // add the students in the test data to the collection
-        addStudent(data);
-        return done();
+        Class.findOne({ section: '096' }, function(error, class_id) {
+            if (error) console.log(error);
+            // add the students in the test data to the collection
+            // Pass the done callback to prevent other tests from
+            // running until the collection is completely loaded.
+            addStudent(data, class_id, done);
+        });     
     });
 
     // Confirm the number of students in the collection
     it('Contains five documents', function(done) {
         Student.find({}, function(error, students) {
             if (error) console.log(error);
-            console.log(students.length);
             expect(students.length).to.equal(5);
+            return done();
         });
-        return done();
     });
 
     // Confirm a student can be found using the unityid
@@ -54,13 +71,34 @@ describe('Student collection: ', function() {
         Student.findOne( {unityid: "rwalker"}, function(error, student) {
             if (error) console.log(error);
             expect(student.lastname).to.equal('Walker');
+            return done();
         });
-        return done();
     });
 
     // Confirm a student can be found using the email property
+    it('Locates a student by email address', function(done) {
+        Student.findOne({ email: 'msmith@ncsu.edu'}, function(error, student) {
+            if (error) console.log(error);
+            expect(student.unityid).to.equal('msmith');
+            return done();
+        });
+    });
 
     // Confirm the sex of the student is a number
+    it('Stores the sex of the student as a number', function(done) {
+        Student.findOne({}, function(error, student) {
+            if (error) console.log(error);
+            expect(student.sex).to.be.a('number');
+            return done();
+        });
+    });
 
-    // Confirm students can be filtered by class
+    // The class is stored for a student
+    it('Retrieves the class property for a student', function(done) {
+        Student.findOne({ email: 'jdoe@ncsu.edu'}, function(error, student) {
+            if (error) console.log(error);
+            expect(student.class).to.have.lengthOf(1);
+            return done();
+        });
+    });
 });

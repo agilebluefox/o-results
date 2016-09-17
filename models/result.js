d3e50ad616558a8fc52749ec0311fd8978150499
David Conner <agilebluefox@gmail.com>
2016-09-16 20:33:10 -0400

    Moved models directory

---
+++ models/result.js
@@ -0,0 +1,19 @@
+'use strict()';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+
+let schema = new Schema( {
+    // Each set of results is connected to an event, course, and student
+    event: { type: Schema.Types.ObjectId, ref: 'Event', required: true }, 
+    course: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
+    card: { type: String, required: true },
+    student: { type: Schema.Types.ObjectId, ref: 'Student', required: true },
+    cn: { type: String },
+    time: { type: Date } 
+});
+
+schema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('Result', schema);
---
+++ models/student.js
@@ -0,0 +1,21 @@
+'use strict()';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+
+let schema = new Schema( {
+    // Unity Id of the student
+    unityid: { type: String, required: true, unique: true },
+    // The email address is the unityId@ncsu.edu
+    email: { type: String, required: true, lowercase: true, unique: true, match: /.+@ncsu.edu/ },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    // Represent female as 0 and male as 1
+    sex: { type: Number, required: true, min: 0, max: 1 },
+    class: [ { type: Schema.Types.ObjectId, ref: 'Class', required: true } ]
+});
+
+schema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('Student', schema);

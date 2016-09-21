e0f10584c4bfa02b4a6c218d2939c4f7dfea3ae2
David Conner <agilebluefox@gmail.com>
2016-09-21 19:06:08 -0400

    Moved function to separate file

--- tests/test-courses.js
+++ tests/test-courses.js
@@ -5,39 +5,12 @@ const expect = require('./setup-tests');
 // Require models
 const Course = require('../models/courses');
 const data = require('./data/test-courses.json');
-
-function getRandomInt(min, max) {
-  Math.ceil(min);
-  Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+const createCodename = require('../libs/codename');
 
 function addCourses(done) {
     let count = 0;
     data.forEach((course) => {
-        let codename = '';
-        const name = course.name.toLowerCase().split(' ');
-
-        name.forEach((word) => {
-            codename += word.substr(0, 1);
-        });
-
-        // Create a date object from the UTC formatted string
-        const date = new Date(course.mapdate);
-
-        // Extract the year, month, and day
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-
-        // generate random number to add to end
-        const min = 1000;
-        const max = 10000;
-        const random = getRandomInt(min, max);
-
-        // Use the intials, date components, and random number
-        // to create a "unique" codename in case I need it later
-        codename += `${year}${month}${day}-${random}`;
+        const codename = createCodename(course.name, course.mapdate);
 
         Course.create({
             location: course.location,

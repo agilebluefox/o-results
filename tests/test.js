06e4c5dfdffcace0666c5efcc114691107aa439a
David Conner <agilebluefox@gmail.com>
2016-09-26 08:53:27 -0400

    Set order for running Mocha tests

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+'use strict()';
+
+require('./test-cards');
+require('./test-controls');
+require('./test-classes');
+require('./test-courses');
+require('./test-events');
+require('./test-students');
+require('./test-results');

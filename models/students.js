fb4ac4d99e89bea938797d54bde6945b13f4bfb6
David Conner <agilebluefox@gmail.com>
2016-12-24 20:22:28 -0500

    Move comments

--- models/students.js
+++ models/students.js
@@ -6,10 +6,8 @@ const Schema = mongoose.Schema;
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 const schema = new Schema({
-    active: { type: Boolean, default: true },
-    // Unity Id of the student
-    unityid: { type: String, required: true, unique: true },
-    // The email address is the unityId@ncsu.edu
+    active: { type: Boolean, default: true }, // Unity Id of the student
+    unityid: { type: String, required: true, unique: true }, // The email address is the unityId@ncsu.edu
     email: { type: String, required: true, lowercase: true, unique: true, match: /.+@ncsu.edu/ },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true }
